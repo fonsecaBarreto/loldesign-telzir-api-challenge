c61a35a5d9636545dca0b5a65ab2c1ac75ffc5af
fonsecaBarreto <lucasbfonte@gmail.com>
2022-08-19 18:44:09 -0300

    chore:  enable cors & set global prefix to app

--- nest/src/main.ts
+++ nest/src/main.ts
@@ -1,8 +1,12 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+
+  app.setGlobalPrefix("api/v1");
+
   await app.listen(3000);
 }
 bootstrap();
