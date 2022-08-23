aff424882a91e81810479fdc0fec53c8bce26a75
fonsecaBarreto <lucasbfonte@gmail.com>
2022-08-23 15:52:04 -0300

    add: swagger

--- nest/src/main.ts
+++ nest/src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
@@ -6,6 +7,16 @@ async function bootstrap() {
   app.enableCors();
 
   app.setGlobalPrefix("api/v1");
+  
+  const config = new DocumentBuilder()
+    .setTitle('Telzir FaleMais')
+    .setDescription('Telzir FaleMais API description')
+    .setVersion('1.0')
+    .addTag('telzir')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
 }
